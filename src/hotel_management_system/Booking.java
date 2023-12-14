e7e4f59199aaa3fe995672ffc01e40485a0d4e7e
Mazen212237 <mazen212237@bue.edu.eg>
2023-12-14 15:52:37 +0200

    Added Extra Classes

--- src/hotel_management_system/Booking.java
+++ src/hotel_management_system/Booking.java
@@ -4,10 +4,64 @@
  */
 package hotel_management_system;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+
+
 /**
  *
  * @author gamer
  */
 class Booking {
+    String BookingID;
+    private static ArrayList<Room> roomList;
+    ArrayList<Room> cart;
+    //date format is year,month,day
+    String startDate;
+    String endDate;
+    
+    Double totalPrice;
+
+
+    //adds room to the cart
+    public void selectRoom(Integer index){
+        
+        if("Available".equals(roomList.get(index).getStatusRoom()))
+        {
+            roomList.get(index).setStatusRoom("Free");
+            cart.add(roomList.get(index));
+        }
+    }
+    
+    
+    //function that Calculate the total from the selected Rooms and duration 
+    public Double CalcTotal(ArrayList<Room> Cart, Integer duration) throws ParseException{
+        for(Room i: Cart){
+            totalPrice+=Duration(i,startDate,endDate);
+        }
+         return  totalPrice;
+        
+    }
+    
+    
+    //calcs the length and cost of stay for selected room
+    public Double Duration(Room room,String start, String end) throws ParseException{
+        //comparing time
+        LocalDate Start=LocalDate.parse(start);
+        LocalDate End=LocalDate.parse(end);
+        Integer Length= (Integer) Start.compareTo(End);
+        // Calc price of selected Room
+        Double Price;
+        Price = (Double) (room.getPrice()*Length);
+        
+        return Price;
+    }
+    
+    
+    
+    
     
 }
